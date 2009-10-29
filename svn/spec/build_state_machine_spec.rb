c32229288cab48797aa455db10d0414e00928393
Martyn Jago <martyn.jago@btinternet.com>
2009-10-29 13:31:36 +0000

    changes to build_state_machine.rb

--- svn/spec/build_state_machine_spec.rb
+++ svn/spec/build_state_machine_spec.rb
@@ -1,75 +1,249 @@
+
 require File.
   expand_path(File.
               join(File.
                    dirname(__FILE__),'..','state','build_state_machine'))
 
-describe StateData do
-  before do
-    @state_data = StateData.new
+describe 'StateMachine' do
+  before(:each) do
+    @s = BuildStateMachine.new('test_state').state_machine
   end
   
-  it 'exists as a class' do
-    @state_data.class.should == StateData
+  it "exists as a class" do
+    BuildStateMachine.new('test_state').class.should == BuildStateMachine
   end
   
-  it 'has a method called test_state_data' do
-    @state_data.methods.include?('test_state_data').should == true
+  it 'returns a instance variable state_machine' do
+    BuildStateMachine.new('test_state').state_machine.class.should == StateMachine
   end
   
-  it 'has a method called connection_state_data' do
-    @state_data.methods.include?('connection_state_data').should == true
-  end
-  
-  it 'has a method called tx_messaging_states_data' do
-    @state_data.methods.include?('dev_tx_messaging_state_data').should == true
+  it 'state data is available via the instance variable data' do
+    @data = BuildStateMachine.new('test_state').data
   end
 
-  describe 'BuildStateMachine' do
-    before(:each) do
-      @statemachine = BuildStateMachine.new
-    end
-    
-    it "exists as a class" do
-      @statemachine.class.should == BuildStateMachine
-    end
-    
-    describe 'build_state_machine' do
-      it 'has a method called build_state_machine' do
-        @statemachine.methods.include?('build_state_machine').should == true
-      end
-      
-      it 'returns a state machine when passed a state machine data name' do
-        @statemachine.build_state_machine('test_state_data').
-          class.should == Statemachine::Statemachine
-      end
-    end    
+  # test StateMachine#climb_to method
 
-    describe 'connection_states' do
-      it 'should have statemachine called connection_states' do
-        @statemachine.connection_states.class.
-          should == Statemachine::Statemachine
+  describe 'StateMachine.climb_to :init_state' do
+    it 'climb_to exists as a public_method of StateMachine' do
+      @s.public_methods.include?('climb_to').should == true
+    end
+
+    describe 'climb_to :init_state' do
+      it 'climbs to init_state when passed init_state' do
+        @s.climb_to :init_state
+        @s.state.should == :init_state
+      end
+
+      it 'climbs to idle_state when passed idle_state' do
+        @s.climb_to :idle_state
+        @s.state.should == :idle_state
       end
       
-      describe 'unconnected_state' do
-        it 'is the initial state' do
-          @statemachine.connection_states.state.should == :unconnected_state
-        end
-        
-        it 'is re-entrant following no_rx_detected!' do
-          @statemachine.connection_states.no_rx_detected!
-          @statemachine.connection_states.state.should == :unconnected_state
-        end
-        
-        it 'changes state to :tx_only_state following tx_detected!' do
-          @statemachine.connection_states.tx_detected!
-          @statemachine.connection_states.state.should == :tx_only_state
-        end
-        
-        it 'changes state to :rx_only_state following rx_detected!' do
-          @statemachine.connection_states.rx_detected!
-          @statemachine.connection_states.state.should == :rx_only_state
-        end
+      it 'climbs to check_comms_state when passed check_comms_state' do
+        @s.climb_to :check_comms_state
+        @s.state.should == :check_comms_state
       end
-	end
+    end
+  end
+  
+  # do some states with test_state_machine...
+  
+  describe 'init_state' do
+    before do
+      @s.climb_to :init_state
+    end
+    
+    it 'is the initial state' do
+      @s.add_node :init_state, :initialised!, :idle_state
+      @s.state.should == :init_state
+    end
+    
+    it 'changes state to :idle_state given initialised!' do
+      @s.initialised!
+      @s.state.should == :idle_state
+    end
+  end
+  
+  describe 'idle_state' do
+    before do
+      @s.climb_to :idle_state
+    end
+
+    it ' remove test_timed_event' do
+      @s.delete_node :idle_state, :test_timed_event!, :test_state
+    end
+    
+    it 'is reentrant given no_data!' do
+      @s.no_data!
+      @s.state.should == :idle_state
+    end
+    
+    it 'changes state to :process_state given data_received!' do
+      @s.data_received!
+      @s.state.should == :process_state
+    end
+
+    it 'changes to shutdown_state given shutdown_request! ' do
+      @s.shutdown_request!
+      @s.state.should == :shutdown_state
+    end
+    
+    it 'changes to log_changes_state given log_changes_timed_event! ' do
+      @s.log_changes_timed_event!
+      @s.state.should == :log_changes_state
+    end
+    
+    it 'changes to check_comms_state given check_comms_timed_event!' do
+      @s.check_comms_timed_event!
+      @s.state.should == :check_comms_state
+    end
+    
+    it 'changes to run_tests_state given run_tests_timed_event!' do
+      @s.add_node :idle_state, :run_tests_timed_event!, :run_tests_state
+      @s.run_tests_timed_event!
+      @s.state.should == :run_tests_state
+    end
+    
+    it 'becomes check_calculations_state given check_calculations_timed_event!' do
+      @s.check_calculations_timed_event!
+      @s.state.should == :check_calculations_state
+    end
+
+    it 'requires check_calculations_timed_event! to be added' do
+      @s.add_node :idle_state, :check_calculations_timed_event!, :check_calculations_state
+    end
+    
+  end
+
+  describe 'check_calculations_state' do
+    before do
+      @s.climb_to :check_calculations_state
+    end
+    
+    it 'becomes check_rate_state given check_rate! ' do
+      @s.check_rate!
+      @s.state.should == :check_rate_state
+    end
+
+    it 'requires check_rate_state' do
+      @s.add_node :check_calculations_state, :check_rate!, :check_rate_state
+    end
+  end
+  
+  describe 'check_rate_state' do
+    before do
+      @s.climb_to :check_rate_state
+    end
+
+    it 'becomes check_volume_state given rate_checked!' do
+      @s.rate_checked!
+      @s.state.should == :check_volume_state
+    end
+    
+    it 'requires the check_volume_state' do
+      @s.add_node :check_rate_state, :rate_checked!, :check_volume_state
+    end
+  end
+  
+  describe 'check_volume_state' do
+    before do
+      @s.climb_to :check_volume_state
+    end
+    
+    it 'becomes idle_state given volume_checked' do
+      @s.volume_checked!
+      @s.state.should == :idle_state
+    end
+
+    it 'requires volume_checked transition' do
+      @s.add_node :check_volume_state, :volume_checked!, :idle_state
+    end
+    
+  end
+    
+  describe 'run_tests_state' do
+    before do
+      @s.climb_to :run_tests_state
+    end
+    
+    it 'remove unwanted node test_state' do
+      @s.delete_node :test_state, :test_running!, :test_state
+    end
+      
+    it 'is reentrant given test_running!' do
+      @s.add_node :run_tests_state, :test_running!, :run_tests_state
+      @s.test_running!
+      @s.state.should == :run_tests_state
+    end
+    
+    it' becomes idle_state given tests completed' do
+      @s.tests_completed!
+      @s.state.should == :idle_state
+    end
+    
+    it 'requires event tests_completed! ' do
+      @s.add_node :run_tests_state, :tests_completed!, :idle_state
+    end
+    
+  end
+  
+  describe 'process_state' do
+    before do
+      @s.climb_to :process_state
+    end
+    
+    it 'changes to idle_state given processed!' do
+      @s.processed!
+      @s.state.should == :idle_state
+    end
+    
+  end
+  
+  describe 'log_changes_state' do
+    before do
+      @s.climb_to :log_changes_state
+    end
+    
+    it 'changes to idle_state given changes_logged!' do
+      @s.changes_logged!
+      @s.state.should == :idle_state
+    end
+    
+  end
+  
+  describe 'check_comms_state' do
+    before do
+      @s.climb_to :check_comms_state
+    end
+    
+    it 'changes to check_comms_state given processing_comms!' do
+      @s.processing_comms!
+      @s.state.should == :check_comms_state
+    end
+    
+    it 'changes to idle_state given comms_processed!' do
+      @s.comms_processed!
+      @s.state.should == :idle_state
+    end
+    
+  end
+  
+  describe 'shutdown_state' do
+    before do
+      @s.climb_to :shutdown_state
+    end
+    
+    it 'is reentrant given shutting_down!' do
+      @s.shutting_down!
+      @s.state.should == :shutdown_state
+     end
+
   end
 end
+
+
+
+
+
+
+
--- svn/state/build_state_machine.rb
+++ svn/state/build_state_machine.rb
@@ -1,48 +1,121 @@
 
 require 'statemachine'
- require File.expand_path(File.join(File.dirname(__FILE__),'stateData','comms_driver_state_data.rb'))
- require File.expand_path(File.join(File.dirname(__FILE__),'stateData','main_state_data.rb'))
- require File.expand_path(File.join(File.dirname(__FILE__),'stateData','state_data.rb'))
  require File.expand_path(File.join(File.dirname(__FILE__),'climb_to'))
- require File.expand_path(File.join(File.dirname(__FILE__),'state_table_generation'))
 
-class BuildStateMachine
-  attr_accessor :connection_states
-  attr_accessor :dev_tx_message_states
-  attr_accessor :dev_rx_message_states
-  attr_accessor :dev_main_states
-  attr_accessor :dev_scheduler_states
-  attr_accessor :tester_tx_message_states
-  attr_accessor :tester_rx_message_states
-  attr_accessor :tester_main_states
-  attr_accessor :tester_scheduler_states
-  attr_accessor :comms_driver_states
-  attr_accessor :main_states
+class StateMachine
+  @sm = nil
+  attr_accessor :state_machine
   
-  attr_accessor :current_states
-  
-  def initialize
-    @connection_states = build_state_machine('connection_state_data')
-    @dev_tx_message_states = build_state_machine('dev_tx_messaging_state_data')
-    @dev_rx_message_states = build_state_machine('dev_rx_messaging_state_data')
-    @dev_main_states = build_state_machine('dev_main_state_data')
-    @dev_scheduler_states = build_state_machine('dev_scheduler_state_data')
-    @tester_tx_message_states = build_state_machine('tester_tx_messaging_state_data')
-    @tester_rx_message_states = build_state_machine('tester_rx_messaging_state_data')
-    @tester_main_states = build_state_machine('tester_main_state_data')
-    @tester_scheduler_states = build_state_machine('tester_scheduler_state_data')
-    @comms_driver_states = build_state_machine('comms_driver_state_data')
-    @main_states = build_state_machine('main_state_data')
+  def initialize(state_machine)
+    @state_machine = state_machine
   end
   
-  def build_state_machine(statemachine_to_build)
-    sm = Statemachine.build do
-      StateData.send(statemachine_to_build).each do |st|
-        trans st[0].to_s.to_sym, st[1].to_s.to_sym, st[2].to_s.to_sym
+  def state
+    @state_machine.state 
+  end
+  
+  def is_transition?(transition)
+    transition.to_s[-1..-1] == '!'
+  end
+    
+  def does_node_exist?(node)
+    state_found = false
+    $data.size.times do |e|
+      if $data[e] == node
+        return true
       end
     end
-    @current_states = sm
-    sm
+    false
+  end
+  
+  def check_node_format node
+    if not node.to_s[-6..-1] == '_state'
+      raise "the #{m} state  is required to end in _state!"
+    end
+  end
+ 
+  def is_node_format?(node)
+    return false if not node.class == Array
+    node.each do |n|
+      return false if not n.class == Symbol
+    end
+    return false if not is_transition? node[STATE_TRANSITION].to_s
+    check_node_format node[FROM_STATE]
+    check_node_format node[TO_STATE]
+  end
+  
+  def add_node(from_state,transition,to_state)
+    node = [from_state, transition, to_state]
+    if is_node_format? node
+      if not does_node_exist?(node)
+        $data.push node
+        self.save
+      end
+    end
+  end
+  
+  def delete_node(from_state, transition, to_state)
+    node = [from_state, transition, to_state]
+    if is_node_format? node
+      if does_node_exist? node
+        $data.delete node 
+        self.save
+      end
+    end
+  end
+  
+  def save
+    File.open(File.
+              expand_path(File.
+                          join(File.
+                               dirname(__FILE__),'stateData',"#{$working_table}.yml")),'w') do |out|
+      YAML.dump($data, out)
+  #    puts $data
+    end
+  end
+  
+  def method_missing(m, *args, &block)
+    if is_transition?(m)
+      @state_machine.send("#{m}")
+    else
+      raise "the transition #{m} is required to end in an exclamation mark!"
+    end
   end
 end
 
+class BuildStateMachine
+  attr_accessor :state_machine
+  attr_accessor :data
+  
+  def initialize(state_machine_name)
+    $working_table = "#{state_machine_name}_data"
+    BuildStateMachine.class.send(:define_method,
+                                 "#{state_machine_name.gsub('state_data','states')}",
+                                 proc { build_state_machine(state_machine_name) } )
+    @state_machine = self.
+      build_state_machine("#{state_machine_name.gsub('state_data','states')}")
+    @state_machine = StateMachine.new(@state_machine)
+  end
+  
+  def build_state_machine(statemachine_to_build)
+    begin
+#      puts "loading #{File.expand_path(File.join(File.dirname(__FILE__),'stateData',"#{$working_table}.yml"))}"
+      $data = YAML::load(File.
+                        read(File.
+                             expand_path(File.
+                                       join(File.
+                                            dirname(__FILE__),'stateData',"#{$working_table}.yml"))))
+    sm = Statemachine.build do
+      $data.each do |st|
+        trans st[0].to_s.to_sym, st[1].to_s.to_sym, st[2].to_s.to_sym
+      end
+    end
+      sm
+    rescue
+      puts "ERROR! non-existant table #{$working_table}.yml"
+      nil
+    end
+  end
+  
+end
+  
