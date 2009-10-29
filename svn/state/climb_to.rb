7002584212883bd8a48a331358d069f31b58404d
Martyn Jago <martyn.jago@btinternet.com>
2009-10-29 13:30:31 +0000

    changes to climb_to.rb

--- svn/state/climb_to.rb
+++ svn/state/climb_to.rb
@@ -3,24 +3,16 @@ FROM_STATE = 0
 STATE_TRANSITION = 1
 TO_STATE = 2
 
-class BuildStateMachine
-
-  CURRENT_DATA = 'main_state_data'
+class StateMachine
   
   def initial_state
-    StateData.send(CURRENT_DATA)[0][0]
-  end
-  
-  def data(data_name)
-    d = StateData.send(data_name.to_s)
-    return d
+    $data[0][0]
   end
   
   def check_TO_STATE?(state)
-    d = data(CURRENT_DATA)
     state_found = false
-    d.size.times do |e|
-      if d[e][TO_STATE] == state
+    $data.size.times do |e|
+      if $data[e][TO_STATE] == state
         return true
       end
     end
@@ -28,31 +20,26 @@ class BuildStateMachine
   end
   
   def fetch_node_leading_to(to_state)
-    d = data(CURRENT_DATA)
-    d.size.times do |x|
-      if(d[x][TO_STATE]== to_state)
-        return d[x]
+    $data.size.times do |x|
+      if($data[x][TO_STATE]== to_state)
+        return $data[x]
       end
     end
     false
   end
     
   def climb_to state_to_climb_to
-    puts "state_to_climb_to"
     @state_to_climb_to = state_to_climb_to
     return false if @state_to_climb_to.class != Symbol
     return false if not check_TO_STATE?(@state_to_climb_to)
     ary = []
-    false
     loop do
       node = fetch_node_leading_to(@state_to_climb_to)
       if not node == false
-        puts node.inspect
         ary << node[STATE_TRANSITION]
-        puts "ary = #{ary}"
         if node[FROM_STATE] == self.initial_state
           ary.reverse.each do |t|
-            @current_states.send(t)
+            @state_machine.send(t)
           end
           return ary.reverse
         end
@@ -62,7 +49,5 @@ class BuildStateMachine
       end
     end
   end
+  
 end
-
-
-
