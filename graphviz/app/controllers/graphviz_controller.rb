ee75203db6769d63bb0af973de1f621e21e42d54
Martyn Jago <martyn.jago@btinternet.com>
2010-04-01 17:12:51 +0100

    state generation

--- graphviz/app/controllers/graphviz_controller.rb
+++ graphviz/app/controllers/graphviz_controller.rb
@@ -21,6 +21,8 @@ class GraphvizController < ApplicationController
     @state_table_h = temp[1]
     @state_routines_c = temp[2]
     @state_routines_h = temp[3]
+    @function_hash_c = temp[4]
+    @function_hash_h = temp[5]
   end
   
   def state_data
@@ -31,6 +33,7 @@ class GraphvizController < ApplicationController
     nodes = []
     from_states = []
     to_states = []
+    transitions = []
     from_state = ""
     to_state = ""
     out_string = "edge[color = green, fontcolor = darkgreen, fontname = \"verdana\", "
@@ -46,18 +49,26 @@ class GraphvizController < ApplicationController
         if line.include? '('
           if line.include? ')'
             from_state = line[0..line.index('->') -2].strip
+            from_state = 'STATE_' + from_state
             from_states << from_state
             line = line[line.index('->') + 2  .. -1].strip
             to_state = line[0..line.index('(') - 1 ].strip
+            to_state = 'STATE_' + to_state
             to_states << to_state
             transition = line[line.index('(') + 1 .. line.index(')') - 1].strip
-            if not nodes.include? from_state
+            if transitions.include? transition
+              flash[:notice] = "ERROR: line #{line_count}, missing \")\": #{line.strip} "
+            else
+              transitions << transition
+            end
+            if not nodes.include? from_state 
               nodes << from_state
-              out_string += "#{from_state} [label = \"  #{from_state.gsub('_','  \n  ')}  \"]\n"
+              puts ("\n\nfrom_state = #{from_state}\n\n")
+              out_string += "#{from_state} [label = \"  #{from_state.gsub("STATE_","").gsub('_','  \n  ')}  \"]\n"
             end
             if not nodes.include? to_state
               nodes << to_state
-              out_string += "#{to_state} [label = \"  #{to_state.gsub('_','  \n  ')}  \"]\n"
+              out_string += "#{to_state} [label = \"  #{to_state.gsub("STATE_","").gsub('_','  \n  ')}  \"]\n"
             end
             out_string += "#{from_state} -> #{to_state}"
             out_string += " [#{from_state == to_state ? 'dir = back, ' : ''}label = \"  #{transition.gsub('_','  \n  ')}!  \"]\n"
@@ -80,13 +91,14 @@ class GraphvizController < ApplicationController
     nodes.uniq!
     nodes.each do |node|
       if not from_states.include?(node) or not to_states.include?(node)
-        out_string += "#{node} [fillcolor = red, label = \"  #{node.gsub('_','  \n  ')}  \"]\n"
+        out_string += "#{node} [fillcolor = red, label = \"  #{node.gsub("STATE_","").gsub('_','  \n  ')}  \"]\n"
       end
-      if node == 'init_state' or node == 'start_state'
-        out_string += "#{node} [fillcolor = cadetblue, label = \"  #{node.gsub('_','  \n  ')}  \"]\n"
+      if node.include?('startup')
+        out_string += "#{node} [fillcolor = black, label = \"X\"]\n"
       end  
     end
     out_string += "\n"
+puts ("\n\nout_string = #{out_string}\n\n")
     out_string
   end
 end
@@ -209,6 +221,54 @@ STATE_ROUTINES_H
 #endif
 STATE_ROUTINES_H
 
+function_hash_c_begin = <<FUNCTION_HASH_C
+#include "function_hash.h"
+#include "types.h"
+
+const flow_hash_t FLOW_source_table[] = 
+{
+FUNCTION_HASH_C
+
+function_hash_c_middle = <<FUNCTION_HASH_C
+};
+
+const sint32_t FLOW_source_offset_table[] =
+{
+FUNCTION_HASH_C
+
+function_hash_c_end = <<FUNCTION_HASH_C
+};
+
+#pragma code
+
+FUNCTION_HASH_C
+
+function_hash_h_begin = <<FUNCTION_HASH_H
+#ifndef _FUNCTION_HASH_H
+#define _FUNCTION_HASH_H
+
+#include "types.h"
+
+typedef enum
+{
+FUNCTION_HASH_H
+
+function_hash_h_middle = <<FUNCTION_HASH_H
+} flow_hash_t;
+
+typedef enum
+{
+FUNCTION_HASH_H
+
+function_hash_h_end = <<FUNCTION_HASH_H
+} flow_t;
+
+extern const FLOW_Hash_t FLOW_hash_table[FLOW_NUM_OF_FUNCTIONS];
+extern const sint32_t FLOW_source_table[];
+extern const sint32_t FLOW_source_offset_table[];
+
+#endif
+FUNCTION_HASH_H
 
   #puts @state_data.inspect
 #   @state_data = [uip_state_data]
@@ -216,31 +276,38 @@ STATE_ROUTINES_H
 #  @state_data += [systick_state_data]
   
   @state_data = state_data
-  @out_state_table_c = state_table_c_begin
-  @out_state_table_h = state_table_h_begin
-  @out_state_routines_c = state_routines_c_begin
-  @out_state_routines_h = state_routines_h_begin
+
+
+@out_state_table_c = state_table_c_begin
+@out_state_table_h = state_table_h_begin
+@out_state_routines_c = state_routines_c_begin
+@out_state_routines_h = state_routines_h_begin
+@out_function_hash_c = function_hash_c_begin
+@out_function_hash_h = function_hash_h_begin
 
   @line_count = 0
   @all_states = []
   @fsm_states= []
   @from_states = []
   @to_states = []
-  @state_data.each do |line|
-    @line_count += 1
+  @state_data.to_s.each_line do |line|
     if line.include? '->'
       from_state = line[0..line.index('->') -2].strip
+      from_state = 'STATE_' + from_state
       line = line[line.index('->') + 2  .. -1].strip
       to_state = line[0..line.index('(') - 1 ].strip
+      to_state = 'STATE_' + to_state
       @fsm_states << from_state
       @fsm_states << to_state
       @fsm_states.uniq!
-        
+      
       @all_states << from_state
       @all_states << to_state
       @all_states.uniq!
+      
       @from_states[@line_count] = from_state
       @to_states[@line_count] = to_state
+      @line_count += 1
     end
   end
   @fsm_states.each do |state|
@@ -251,8 +318,44 @@ STATE_ROUTINES_H
     @out_state_routines_c += "}\n"
     @out_state_routines_h += "state_t #{state}(void);\n"
     @out_state_table_c += "        #{state},\n"
-    @out_state_table_h += "    #{state.upcase} = #{get_SHA2(state)},\n"
+    @out_state_table_h += "    #{state.upcase},\n"
+    @out_function_hash_h += "    #{state.upcase.gsub('STATE_','FLOW_HASH_')} = #{get_SHA2(state)},\n"
   end
+
+  @out_function_hash_h += function_hash_h_middle
+
+  @dest_states = []
+  @state_positions = []
+  @current_state = ""
+  @source_count = 0
+  @all_states.each do |state|
+    source_count_logged = false
+    0.upto(@from_states.length - 1) do |count| 
+      if state == @from_states[count]
+        if not source_count_logged
+          @state_positions << count
+          source_count_logged = true
+        end
+        @dest_states << @to_states[count] 
+      end
+    end
+  end
+
+  @dest_states.each do |state|
+    @out_function_hash_c += "    #{state.upcase.gsub('STATE_','FLOW_HASH_')},\n"
+  end
+
+  @out_function_hash_c += function_hash_c_middle
+
+  @state_positions.each do |position|
+    @out_function_hash_c += "    #{position},\n"
+  end
+
+  @fsm_states.each do |state|
+    @out_function_hash_h += "    #{state.upcase.gsub('STATE_','FLOW_')},\n"
+  end
+
+
   @out_state_table_h += "\n"
   @out_state_table_c +=   "\n"
   @out_state_routines_h += "\n"
@@ -260,10 +363,129 @@ STATE_ROUTINES_H
   @out_state_routines_h += state_routines_h_end   
   @out_state_table_h += "    STATE_NUM_OF_STATES = #{@all_states.size}\n"
   @out_state_table_h += state_table_h_end
+  @out_function_hash_c += function_hash_c_end
+  @out_function_hash_h += function_hash_h_end
+
   [
    @out_state_table_c,
    @out_state_table_h,
    @out_state_routines_c,
-   @out_state_routines_h
+   @out_state_routines_h,
+   @out_function_hash_c,
+   @out_function_hash_h
   ]
-end
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+#   @out_state_table_c = state_table_c_begin
+#   @out_state_table_h = state_table_h_begin
+#   @out_state_routines_c = state_routines_c_begin
+#   @out_state_routines_h = state_routines_h_begin
+
+#   @line_count = 0
+#   @all_states = []
+#   @fsm_states= []
+#   @from_states = []
+#   @to_states = []
+#   @state_data.each do |line|
+#     @line_count += 1
+#     if line.include? '->'
+#       from_state = line[0..line.index('->') -2].strip
+#       from_state = 'STATE_' + from_state
+#       line = line[line.index('->') + 2  .. -1].strip
+#       to_state = line[0..line.index('(') - 1 ].strip
+#       to_state = 'STATE_' + to_state
+#       @fsm_states << from_state
+#       @fsm_states << to_state
+#       @fsm_states.uniq!
+        
+#       @all_states << from_state
+#       @all_states << to_state
+#       @all_states.uniq!
+#       @from_states[@line_count] = from_state
+#       @to_states[@line_count] = to_state
+#     end
+#   end
+#   @fsm_states.each do |state|
+#     @out_state_routines_c += "\nstate_t #{state}(void)\n"
+#     @out_state_routines_c += "{\n    FLOW_check(#{state.upcase});\n\n"
+#     @out_state_routines_c += "    \\* code goes here *\\\n\n"
+#     @out_state_routines_c += "    return(#{state.upcase});\n"
+#     @out_state_routines_c += "}\n"
+#     @out_state_routines_h += "state_t #{state}(void);\n"
+#     @out_state_table_c += "        #{state},\n"
+#     @out_state_table_h += "    #{state.upcase} = #{get_SHA2(state)},\n"
+#   end
+#   @out_state_table_h += "\n"
+#   @out_state_table_c +=   "\n"
+#   @out_state_routines_h += "\n"
+#   @out_state_table_c += state_table_c_end   
+#   @out_state_routines_h += state_routines_h_end   
+#   @out_state_table_h += "    STATE_NUM_OF_STATES = #{@all_states.size}\n"
+#   @out_state_table_h += state_table_h_end
+#   [
+#    @out_state_table_c,
+#    @out_state_table_h,
+#    @out_state_routines_c,
+#    @out_state_routines_h
+#   ]
+#end
--- graphviz/state_machine_generation.rb
+++ graphviz/state_machine_generation.rb
@@ -14,28 +14,40 @@ def get_SHA2(state)
 end
 
 uip_state_data = <<UIP_STATE_DATA
-STATE_uip_init -> STATE_uip_idle (transition_1)
-STATE_uip_idle -> STATE_uip_idle (transition_2)
-STATE_uip_idle -> STATE_uip_process (transition_3)
-STATE_uip_process -> STATE_uip_process (transition_4)
-STATE_uip_process -> STATE_uip_idle  (transition_5)
+uip_init -> uip_idle (transition_1)
+uip_idle -> uip_idle (transition_2)
+uip_idle -> uip_process (transition_3)
+uip_process -> uip_process (transition_4)
+uip_process -> uip_idle  (transition_5)
 UIP_STATE_DATA
 
 systick_state_data = <<SYSTICK_STATE_DATA
-STATE_systick_init -> STATE_systick_idle (transition_1)
-STATE_systick_idle -> STATE_systick_idle (transition_2)
-STATE_systick_idle -> STATE_systick_process (transition_3)
-STATE_systick_process -> STATE_systick_process (transition_4)
-STATE_systick_process -> STATE_systick_idle  (transition_5)
+systick_init -> systick_idle (transition_1)
+systick_idle -> systick_idle (transition_2)
+systick_idle -> systick_process (transition_3)
+systick_process -> systick_process (transition_4)
+systick_process -> systick_idle  (transition_5)
 SYSTICK_STATE_DATA
 
 usb_state_data = <<USB_STATE_DATA
-STATE_usb_init -> STATE_usb_idle (transition_1)
-STATE_usb_idle -> STATE_usb_idle (transition_2)
-STATE_usb_idle -> STATE_usb_process (transition_3)
-STATE_usb_process -> STATE_usb_process (transition_4)
-STATE_usb_process -> STATE_usb_idle  (transition_5)
+usb_init -> usb_idle (transition_1)
+usb_idle -> usb_idle (transition_2)
+usb_idle -> usb_process (transition_3)
+usb_process -> usb_process (transition_4)
+usb_process -> usb_idle  (transition_5)
 USB_STATE_DATA
+
+state_table_c_begin = <<FLOW_VALIDATION_C
+const state_t FLOW_validation_table[] =
+{
+  
+FLOW_VALIDATION_C
+
+state_table_c_end = <<FLOW_VALIDATION_C
+
+#pragma code
+
+FLOW_VALIDATION_C
 
 state_table_c_begin = <<STATE_TABLE_C
 \\* state_table.c... *\\
@@ -115,6 +127,107 @@ state_routines_h_end = <<STATE_ROUTINES_H
 #endif
 STATE_ROUTINES_H
 
+function_hash_c_begin = <<FUNCTION_HASH_C
+#include "function_hash.h"
+#include "types.h"
+
+const flow_hash_t FLOW_source_table[] = 
+{
+FUNCTION_HASH_C
+
+function_hash_c_middle = <<FUNCTION_HASH_C
+};
+
+const sint32_t FLOW_source_offset_table[] =
+{
+FUNCTION_HASH_C
+
+function_hash_c_end = <<FUNCTION_HASH_C
+};
+
+#pragma code
+
+FUNCTION_HASH_C
+
+function_hash_h_begin = <<FUNCTION_HASH_H
+#ifndef _FUNCTION_HASH_H
+#define _FUNCTION_HASH_H
+
+#include "types.h"
+
+typedef enum
+{
+FUNCTION_HASH_H
+
+function_hash_h_middle = <<FUNCTION_HASH_H
+} flow_hash_t;
+
+typedef enum
+{
+FUNCTION_HASH_H
+
+function_hash_h_end = <<FUNCTION_HASH_H
+} flow_t;
+
+extern const FLOW_Hash_t FLOW_hash_table[FLOW_NUM_OF_FUNCTIONS];
+extern const sint32_t FLOW_source_table[];
+extern const sint32_t FLOW_source_offset_table[];
+
+#endif
+FUNCTION_HASH_H
+
+
+
+
+
+
+
+
+
+
+    #~ FLOW_FN1_HASH = 0x12345678,
+    #~ FLOW_FN2_HASH = 0x22345678,
+    #~ FLOW_FN3_HASH = 0x32345678,
+    #~ FLOW_FN4_HASH = 0x42345678,
+  #~ } FLOW_Function_Hashes_t;
+
+#~ typedef enum
+  #~ {
+    #~ FLOW_FN1 = 0,
+    #~ FLOW_FN2,
+    #~ FLOW_FN3,
+    #~ FLOW_FN4,
+    #~ FLOW_NUM_OF_FUNCTIONS
+  #~ } FLOW_Functions_t;
+
+#~ extern const FLOW_Hash_t FLOW_hash_table[FLOW_NUM_OF_FUNCTIONS];
+#~ extern const sint32_t FLOW_source_table[];
+#~ extern const sint32_t FLOW_source_offset_table[];
+
+#~ #endif
+#~ FUNCTION_HASH_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #puts @state_data.inspect
 @state_data = [uip_state_data]
@@ -125,62 +238,192 @@ STATE_ROUTINES_H
 @out_state_table_h = state_table_h_begin
 @out_state_routines_c = state_routines_c_begin
 @out_state_routines_h = state_routines_h_begin
+@out_function_hash_c = function_hash_c_begin
+@out_function_hash_h = function_hash_h_begin
 
 @line_count = 0
 @all_states = []
+@fsm_states= []
 @from_states = []
 @to_states = []
-@state_data.each do |fsm|
-  @fsm_states= []
-  fsm.each_line do |line|
-    @line_count += 1
-    if line.include? '->'
-      from_state = line[0..line.index('->') -2].strip
-      line = line[line.index('->') + 2  .. -1].strip
-      to_state = line[0..line.index('(') - 1 ].strip
-      @fsm_states << from_state
-      @fsm_states << to_state
-      @fsm_states.uniq!
+@state_data.to_s.each_line do |line|
+  if line.include? '->'
+    from_state = line[0..line.index('->') -2].strip
+    from_state = 'STATE_' + from_state
+    line = line[line.index('->') + 2  .. -1].strip
+    to_state = line[0..line.index('(') - 1 ].strip
+    to_state = 'STATE_' + to_state
+    @fsm_states << from_state
+    @fsm_states << to_state
+    @fsm_states.uniq!
       
-      @all_states << from_state
-      @all_states << to_state
-      @all_states.uniq!
-      @from_states[@line_count] = from_state
-      @to_states[@line_count] = to_state
+    @all_states << from_state
+    @all_states << to_state
+    @all_states.uniq!
+    
+    @from_states[@line_count] = from_state
+    @to_states[@line_count] = to_state
+    @line_count += 1
+  end
+end
+@fsm_states.each do |state|
+  @out_state_routines_c += "\nstate_t #{state}(void)\n"
+  @out_state_routines_c += "{\n    FLOW_check(#{state.upcase});\n\n"
+  @out_state_routines_c += "    \\* code goes here *\\\n\n"
+  @out_state_routines_c += "    return(#{state.upcase});\n"
+  @out_state_routines_c += "}\n"
+  @out_state_routines_h += "state_t #{state}(void);\n"
+  @out_state_table_c += "        #{state},\n"
+  @out_state_table_h += "    #{state.upcase},\n"
+  @out_function_hash_h += "    #{state.upcase.gsub('STATE_','FLOW_HASH_')} = #{get_SHA2(state)},\n"
+end
+
+@out_function_hash_h += function_hash_h_middle
+
+@dest_states = []
+@state_positions = []
+@current_state = ""
+@source_count = 0
+@all_states.each do |state|
+  source_count_logged = false
+  0.upto(@from_states.length - 1) do |count| 
+    if state == @from_states[count]
+      if not source_count_logged
+        @state_positions << count
+        source_count_logged = true
+      end
+      @dest_states << @to_states[count] 
     end
   end
-  @fsm_states.each do |state|
-    @out_state_routines_c += "\nstate_t #{state}(void)\n"
-    @out_state_routines_c += "{\n    FLOW_check(#{state.upcase});\n\n"
-    @out_state_routines_c += "    \\* code goes here *\\\n\n"
-    @out_state_routines_c += "    return(#{state.upcase});\n"
-    @out_state_routines_c += "}\n"
-    @out_state_routines_h += "state_t #{state}(void);\n"
-    @out_state_table_c += "        #{state},\n"
-    @out_state_table_h += "    #{state.upcase} = #{get_SHA2(state)},\n"
-  end
-  @out_state_table_h += "\n"
-  @out_state_table_c +=   "\n"
-  @out_state_routines_h += "\n"
 end
+
+@dest_states.each do |state|
+  @out_function_hash_c += "    #{state.upcase.gsub('STATE_','FLOW_HASH_')},\n"
+end
+
+@out_function_hash_c += function_hash_c_middle
+
+@state_positions.each do |position|
+  @out_function_hash_c += "    #{position},\n"
+end
+
+@fsm_states.each do |state|
+  @out_function_hash_h += "    #{state.upcase.gsub('STATE_','FLOW_')},\n"
+end
+
+
+@out_state_table_h += "\n"
+@out_state_table_c +=   "\n"
+@out_state_routines_h += "\n"
 @out_state_table_c += state_table_c_end   
 @out_state_routines_h += state_routines_h_end   
 @out_state_table_h += "    STATE_NUM_OF_STATES = #{@all_states.size}\n"
 @out_state_table_h += state_table_h_end
+@out_function_hash_c += function_hash_c_end
+@out_function_hash_h += function_hash_h_end
+
+[
+ @out_state_table_c,
+ @out_state_table_h,
+ @out_state_routines_c,
+ @out_state_routines_h
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#~ |
+#~ @out_state_table_c = state_table_c_begin
+#~ @out_state_table_h = state_table_h_begin
+#~ @out_state_routines_c = state_routines_c_begin
+#~ @out_state_routines_h = state_routines_h_begin
+
+#~ @line_count = 0
+#~ @all_states = []
+#~ @from_states = []
+#~ @to_states = []
+#~ @state_data.each do |fsm|
+  #~ @fsm_states= []
+  #~ fsm.each_line do |line|
+    #~ @line_count += 1
+    #~ if line.include? '->'
+      #~ from_state = line[0..line.index('->') -2].strip
+      #~ line = line[line.index('->') + 2  .. -1].strip
+      #~ to_state = line[0..line.index('(') - 1 ].strip
+      #~ @fsm_states << from_state
+      #~ @fsm_states << to_state
+      #~ @fsm_states.uniq!
+      
+      #~ @all_states << from_state
+      #~ @all_states << to_state
+      #~ @all_states.uniq!
+      #~ @from_states[@line_count] = from_state
+      #~ @to_states[@line_count] = to_state
+    #~ end
+  #~ end
+  #~ @fsm_states.each do |state|
+    #~ @out_state_routines_c += "\nstate_t #{state}(void)\n"
+    #~ @out_state_routines_c += "{\n    FLOW_check(#{state.upcase});\n\n"
+    #~ @out_state_routines_c += "    \\* code goes here *\\\n\n"
+    #~ @out_state_routines_c += "    return(#{state.upcase});\n"
+    #~ @out_state_routines_c += "}\n"
+    #~ @out_state_routines_h += "state_t #{state}(void);\n"
+    #~ @out_state_table_c += "        #{state},\n"
+    #~ @out_state_table_h += "    #{state.upcase} = #{get_SHA2(state)},\n"
+  #~ end
+  #~ @out_state_table_h += "\n"
+  #~ @out_state_table_c +=   "\n"
+  #~ @out_state_routines_h += "\n"
+#~ end
+#~ @out_state_table_c += state_table_c_end   
+#~ @out_state_routines_h += state_routines_h_end   
+#~ @out_state_table_h += "    STATE_NUM_OF_STATES = #{@all_states.size}\n"
+#~ @out_state_table_h += state_table_h_end
 
 puts
 
-16.times { print '-   '}; puts
-puts @out_state_table_c
+#~ 16.times { print '-   '}; puts
+#~ puts @out_state_table_c
+
+#~ 16.times { print '-   '}; puts
+#~ puts @out_state_table_h
+
+#~ 16.times { print '-   '}; puts
+#~ puts @out_state_routines_c
+
+#~ 16.times { print '-   '}; puts
+#~ puts @out_state_routines_h
 
 16.times { print '-   '}; puts
-puts @out_state_table_h
-
-16.times { print '-   '}; puts
-puts @out_state_routines_c
-
-16.times { print '-   '}; puts
-puts @out_state_routines_h
+puts @out_function_hash_c
 
 16.times { print '-   '}; puts
 
